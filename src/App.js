c89ad5de28e89eea22c1b6e00f30a8eb995a4aed
unknown <bkpandey.pandey@gmail.com>
2022-09-21 16:29:11 +0530

    nobel react app uploaded succesfunlly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Add() {
+    let a = parseInt(prompt('Enter a value'));
+    let b = parseInt(prompt('Enter b value'));
+    let c = a+b;
+    alert('The additions of proggrames is :' + c)
 }
 
-export default App;
+function Subs() {
+    
+    let a = parseInt(prompt('Enter a value'));
+    let b = parseInt(prompt('Enter b value'));
+    let c = a - b;
+    alert('The substraction  of proggrames is :' + c)
+}
+
+export { Add,Subs }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Add,Subs} from './App';
+import './style.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("nobel-app-calc"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <>
+    <button type ="button" id="btn" getElementById onClick={Add}> Click
+    for additions</button>
+     <br/> <br/>
+    <button type ="button" id="btn"
+    onClick={ Subs }> Click for substraction</button> 
+
+    </>
+
+)
